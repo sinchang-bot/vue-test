9d08c9f51181c10fb243515dcd79d4c7feebb354
sinchang <sinchangwen@gmail.com>
2018-02-05 14:48:58 +0800

    lesson-3

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,12 +1,12 @@
 <template>
-    <li class="message">{{message}}</li>
+    <li style="margin-top: 10px" class="message">{{message}}</li>
 </template>
 
 <script>
   export default {
     props: ['message'],
     created() {
-      console.log('CREATED!')
+      // console.log('CREATED!')
     }
   }
 </script>
--- src/components/MessageList.test.js
+++ src/components/MessageList.test.js
@@ -1,6 +1,7 @@
 import { mount } from '@vue/test-utils'
 
 import MessageList from './MessageList.vue'
+import Message from './Message.vue'
 
 describe('MessageList.test.js', () => {
   let cmp
@@ -20,5 +21,45 @@ describe('MessageList.test.js', () => {
 
   it('has the expected html structure', () => {
     expect(cmp.element).toMatchSnapshot()
+  })
+
+  it('is a MessageList component', () => {
+    expect(cmp.is(MessageList)).toBe(true)
+
+    // Or with CSS selector
+    expect(cmp.is('ul')).toBe(true)
+  })
+
+  it('contains a Message component', () => {
+    expect(cmp.contains(Message)).toBe(true)
+
+    // Or with CSS selector
+    expect(cmp.contains('.message')).toBe(true)
+  })
+
+  it('Both MessageList and Message are vue instances', () => {
+    expect(cmp.isVueInstance()).toBe(true)
+    expect(cmp.find(Message).isVueInstance()).toBe(true)
+  })
+
+  it('Message element exists', () => {
+    expect(cmp.find('.message').exists()).toBe(true)
+  })
+
+  it('Message is not empty', () => {
+    expect(cmp.find(Message).isEmpty()).toBe(false)
+  })
+
+  it('Message has a class attribute set to "message"', () => {
+    // For asserting "class", the `hasClass` method is easier
+    expect(cmp.find(Message).attributes().class).toBe('message')
+  })
+
+  it('Message component has the .message class', () => {
+    expect(cmp.find(Message).classes()).toContain('message')
+  })
+
+  it('Message component has style padding-top: 10', () => {
+    expect(cmp.find(Message).element.style._values['margin-top']).toEqual('10px')
   })
 })
